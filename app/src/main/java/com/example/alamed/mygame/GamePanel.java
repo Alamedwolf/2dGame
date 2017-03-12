d635ffec94eaf17d302232a6398b738fdff6a8f0
Nick <nh.ortizm@hotmail.com>
2017-03-12 01:00:00 -0300

    MainThread class added

--- app/src/main/java/com/example/alamed/mygame/GamePanel.java
+++ app/src/main/java/com/example/alamed/mygame/GamePanel.java
@@ -1,6 +1,7 @@
 package com.example.alamed.mygame;
 
 import android.content.Context;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -8,23 +9,59 @@ import android.view.SurfaceView;
  * Created by Nicolás on 11-03-2017.
  */
 
+
+
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback
 {
+    private MainThread thread;
     public GamePanel(Context context)
     {
         super(context);
 
+
         // add the callback to the SurfaceHolder to intercepts events
         getHolder().addCallback(this);
+
+        thread = new MainThread(getHolder(), this);
+
+        //make gamePanel focusable so it can handle events
+        setFocusable(true);
     }
 
   @Override
   public  void  surfaceChanged(SurfaceHolder holder,int format, int width, int height){}
 
   @Override
-  public void  surfaceDestroyed(SurfaceHolder holder){}
+  public void  surfaceDestroyed(SurfaceHolder holder)
+  {
+      boolean retry = true;
+      while (retry)
+      {
+          try {
+              thread.setRunning(false);
+              thread.join();
+          }catch(InterruptedException e){e.printStackTrace();}
+          retry = false;
+      }
+  }
 
   @Override
-   public  void surfaceCreated(SurfaceHolder holder){}
+   public  void surfaceCreated(SurfaceHolder holder){
+
+      //safely start the game loop
+      thread.setRunning(true);
+      thread.start();
+  }
+
+  @Override
+   public  boolean onTouchEvent(MotionEvent event)
+  {
+      return super.onTouchEvent(event);
+  }
+
+   public void update()
+   {
+
+   }
 
 }
